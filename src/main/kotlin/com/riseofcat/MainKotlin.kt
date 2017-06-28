91a682e30a79a7d18cf01d078b8cdd20fa17f6c4
dim <avdim@mail.ru>
2017-06-28 18:38:02 +0300

    ktor

--- src/main/kotlin/com/riseofcat/MainKotlin.kt
+++ src/main/kotlin/com/riseofcat/MainKotlin.kt
@@ -1,13 +1,53 @@
 package com.riseofcat
 
+import org.jetbrains.ktor.application.Application
+import org.jetbrains.ktor.application.call
+import org.jetbrains.ktor.application.install
+import org.jetbrains.ktor.features.ConditionalHeaders
+import org.jetbrains.ktor.features.DefaultHeaders
+import org.jetbrains.ktor.features.PartialContentSupport
+import org.jetbrains.ktor.host.embeddedServer
+import org.jetbrains.ktor.jetty.Jetty
+import org.jetbrains.ktor.routing.Routing
+import org.jetbrains.ktor.routing.get
+import org.jetbrains.ktor.websocket.Frame
+import org.jetbrains.ktor.websocket.webSocket
 import spark.Spark
-import spark.Spark.*
+import spark.Spark.get
 
 object MainKotlin {
-  @JvmStatic fun main(args: Array<String>) {
-    System.getenv("PORT")?.let {
-      Spark.port(it.toInt())
+    @JvmStatic fun main(args: Array<String>) {
+        if(true) {
+            var port: Int = 5000
+            try {
+                port = Integer.valueOf(System.getenv("PORT"))
+            } catch(e: Exception) {
+
+            }
+            embeddedServer(Jetty, port, reloadPackages = listOf("heroku"), module = Application::module/*, host = "localhost"*/).start()
+        } else {
+            System.getenv("PORT")?.let {
+                Spark.port(it.toInt())
+            }
+            get("/") { req, res -> "Hello from Kotlin" }
+        }
     }
-    get("/") { req, res -> "Hello from Kotlin" }
-  }
 }
+
+fun Application.module() {
+    install(DefaultHeaders)
+    install(ConditionalHeaders)
+    install(PartialContentSupport)
+
+    install(Routing) {
+        webSocket("/socket") {
+            //https://github.com/Kotlin/ktor/blob/master/ktor-samples/ktor-samples-websocket/src/org/jetbrains/ktor/samples/chat/ChatApplication.kt
+            this.send(Frame.Text("hello from ktor websocket"))
+        }
+        get("/") {
+            call.respond("hi")
+        }
+    }
+}
+
+
