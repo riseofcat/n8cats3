e8cc94f47373b2545fbadf68fd9766461ee28170
dim <avdim@mail.ru>
2017-07-16 18:48:09 +0300

    -log

--- src/main/java/com/riseofcat/App.java
+++ src/main/java/com/riseofcat/App.java
@@ -1,31 +1,30 @@
 package com.riseofcat;
 
 import com.n8cats.lib_gwt.Const;
-import com.n8cats.lib_gwt.ILog;
 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 
 public class App {
 //private static Logger logger = LoggerFactory.getLogger("main");
-public static ILog log = new ILog() {
-	@Override
-	public String info(String s) {
-//		logger.info(s);
-//		java.lang.System.out.println(s);
-		return s;
-	}
-	@Override
-	public void error(String s) {
-//		logger.error(s);
-//		java.lang.System.out.println("error " + s);
-	}
-	@Override
-	public void warning(String s) {
-//		logger.warn(s);
-//		java.lang.System.out.println("warning " + s);
-	}
-};
+//public static ILog log = new ILog() {
+//	@Override
+//	public String info(String s) {
+////		logger.info(s);
+////		java.lang.System.out.println(s);
+//		return s;
+//	}
+//	@Override
+//	public void error(String s) {
+////		logger.error(s);
+////		java.lang.System.out.println("error " + s);
+//	}
+//	@Override
+//	public void warning(String s) {
+////		logger.warn(s);
+////		java.lang.System.out.println("warning " + s);
+//	}
+//};
 public static final Info info = new Info();
 
 }
